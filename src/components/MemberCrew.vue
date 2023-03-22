8b6f42a94c072dbb409cdf7e947e9aedd133cd58
RochaMoran <rochamoran1@gmail.com>
2023-03-21 19:33:05 -0700

    REFACTOR: crew carrousel style

--- src/components/MemberCrew.vue
+++ src/components/MemberCrew.vue
@@ -1,10 +1,10 @@
 <template>
     <article class="section crew-member" :class="{'crew-member__active': isActive}">
+      <img class="crew-member__image" :src="member.image" :alt="member.name">
       <div class="container-info">
         <span class="crew-member__name">{{ member.name }}</span>
         <p class="crew-member__position">{{ member.rol }}</p>
       </div>
-      <img class="crew-member__image" :src="member.image" :alt="member.name">
     </article>
   </template>
   
